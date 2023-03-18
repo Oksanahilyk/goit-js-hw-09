089cbaae66207101e69fce700721348eea01a08a
Oksanahilyk <hilyk@ukr.net>
2023-03-18 12:20:17 +0200

    add task01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
 
+const startColor = document.querySelector('button[data-start]');
+const stopColor = document.querySelector('button[data-stop]');
+const bodyColor = document.querySelector('body');
+
+let timerId = null;
+
+startColor.addEventListener('click', onStartColor); 
+stopColor.addEventListener('click', onStopColor);
+
+
+function onStartColor() {
+  timerId = setInterval(getBgColor, 1000);
+  startColor.setAttribute('disabled', true);
+  stopColor.removeAttribute('disabled');
+}
+
+function onStopColor() {
+  clearInterval(timerId);
+  startColor.removeAttribute('disabled');
+  stopColor.setAttribute('disabled', true);
+}
+
+function getBgColor() {
+  bodyColor.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+	return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+ }
